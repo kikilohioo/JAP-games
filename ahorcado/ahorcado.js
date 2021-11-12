a12eb1e4c74e66805fb5e92af4a072ea9f897397
Caiqui Viera <56409325+kikilohioo@users.noreply.github.com>
2021-11-12 13:56:49 -0300

    Update ahorcado.js

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -87,6 +87,7 @@ function comprobarLetra(){
                     ];
                     window.localStorage.setItem("puntuaciones", JSON.stringify(puntuaciones));
                 }
+                document.getElementById("puntuacion").innerHTML = JSON.parse(window.localStorage.getItem("puntuaciones")).juegos.ahorcado;
                 document.getElementById("alert-container").innerHTML =
                 `<div class="alert alert-success d-flex align-items-center" role="alert">
                     <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
